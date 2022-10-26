9ba081b49cfbf1c24e9a8f30e18f6304438e68de
Tauhee-d <110287045+Tauhee-d@users.noreply.github.com>
2022-10-26 17:29:58 +0530

    removed comments

--- routes/routeUser.js
+++ routes/routeUser.js
@@ -2,6 +2,7 @@ const express = require("express");
 const router = express.Router();
 const modelUser = require("../models/mUser");
 const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
 
 router.post("/signup", async (req, res) => {
   const { name, email, password, confirmPassword } = req.body;
@@ -20,15 +21,24 @@ router.post("/signup", async (req, res) => {
       });
       return;
     }
-    const encrpt = bcrypt.hashSync(password, 15);
-    const saveUser = new modelUser({
-      name: name,
-      email: email,
-      password: encrpt,
-    });
-    await saveUser.save();
-    res.status(201).send({
-      message: "Registrattion Done",
+
+    bcrypt.hash(password, 15, function (err, hash) {
+      if (err) {
+        return res.send({
+          message: "something wrong, Try later",
+          error: err,
+        });
+      } else {
+        const saveUser = new modelUser({
+          name: name,
+          email: email,
+          password: hash,
+        });
+        saveUser.save();
+        res.status(201).send({
+          message: "Registrattion Done",
+        });
+      }
     });
   } catch (err) {
     res.status(500).send({
@@ -37,6 +47,7 @@ router.post("/signup", async (req, res) => {
   }
 });
 
+// login
 router.post("/signin", async (req, res) => {
   const { email, password } = req.body;
   if (!email || !password) {
@@ -44,6 +55,35 @@ router.post("/signin", async (req, res) => {
       message: "missing required values",
     });
     return;
+  }
+  const filter = { email: email };
+  try {
+    const user = await modelUser.findOne(filter);
+    if (!user) {
+      res.status(404).send({
+        error: "no user with this email",
+      });
+      return;
+    }
+    bcrypt.compare(password, user.password, function (err, result) {
+      if (result) {
+        const token = jwt.sign({ id: user._id }, "secret", {
+          expiresIn: "365 days",
+        });
+        res.status(200).send({
+          token: token,
+          message: "Sucessfull loged in",
+        });
+      } else {
+        res.status(406).send({
+          error: "password does not match",
+        });
+      }
+    });
+  } catch (error) {
+    res.status(500).send({
+      error: error.message,
+    });
   }
 });
 
